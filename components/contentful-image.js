80a34d06e3bdb36f2693044494c7dfeb446aa1af
zzulanas <zzulanas@gmail.com>
2022-11-06 23:30:38 -0500

    sizing corect, need to fix responsive design

--- components/contentful-image.js
+++ components/contentful-image.js
@@ -1,11 +1,27 @@
 import Image from 'next/image'
+import { useState } from 'react';
 
 const contentfulLoader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 100}`
 }
 
+function cn(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
 const ContentfulImage = (props) => {
-  return <Image loader={contentfulLoader} {...props} />
+  const [isLoading, setLoading] = useState(true);
+  return <Image 
+          loader={contentfulLoader} 
+          {...props}  
+          className={cn(
+            'group-hover:opacity-75 duration-700 ease-in-out',
+            isLoading
+              ? 'grayscale blur-2xl scale-110'
+              : 'grayscale-0 blur-0 scale-100',
+              "w-full aspect-w-1 aspect-h-1 bg-gray-200 overflow-hidden xl:aspect-w-7 xl:aspect-h-8"
+          )}
+  onLoadingComplete={() => setLoading(false)}/>
 }
 
 export default ContentfulImage
--- components/gallery.js
+++ components/gallery.js
@@ -4,26 +4,13 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Gallery({ pictures }){
-    const layoutSizeA = "w-96 h-96 relative overflow-hidden col-span-1 row-span-1 mb-10"
-    const layoutSize2ARow = "w-192 h-96 relative overflow-hidden col-span-2 row-span-1 mb-10"
-    const layoutSize2ACol = "w-96 h-192 relative overflow-hidden col-span-1 row-span-2 mb-10"
-    const imageStyle = "`m-10 hover:shadow-lg hover:transition ease-in-out duration-300 object-center hover:blur-md"
+    const layoutSizeA = "relative overflow-hidden object-cover"
+    const layoutSize2ARow = "relative overflow-hidden col-span-2 sm:col-span-1"
+    const layoutSize2ACol = "relative overflow-hidden row-span-2 sm:row-span-1"
+
+    const textStyle = "opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-4xl text-white font-semibold"
     return(
-    <div className="grid grid-cols-3 auto-rows-auto">
-        {/* {pictures.map((picture, idx) => (
-                <div className={layoutSize2ACol}>
-                    <Link href={`/projects/${picture.slug}`} key={picture.title}>
-                        <a>
-                            <ContentfulImage
-                                layout="fill"
-                                objectFit="cover"
-                                src={picture.url}
-                                className={`m-10 hover:shadow-lg hover:transition ease-in-out duration-300 object-center`}
-                            />
-                        </a>
-                    </Link>
-                    </div>           
-        ))} */}
+    <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
         <div className={layoutSizeA}>
             <Link href={`/projects/${pictures[0].slug}`} key={pictures[0].title}>
                 <a>
@@ -31,8 +18,9 @@ export default function Gallery({ pictures }){
                         layout="fill"
                         objectFit="cover"
                         src={pictures[0].url}
-                        className={imageStyle}
                     />
+                    <img src="https://via.placeholder.com/400x400" className="display: none;"/>
+                    <div className={textStyle}>{pictures[0].title}</div>
                 </a>
             </Link>
         </div> 
@@ -43,8 +31,9 @@ export default function Gallery({ pictures }){
                         layout="fill"
                         objectFit="cover"
                         src={pictures[1].url}
-                        className={imageStyle}
                     />
+                    <img src="https://via.placeholder.com/400x400" className="display: none;"/>
+                    <div className={textStyle}>{pictures[1].title}</div>
                 </a>
             </Link>
         </div> 
@@ -55,8 +44,9 @@ export default function Gallery({ pictures }){
                         layout="fill"
                         objectFit="cover"
                         src={pictures[2].url}
-                        className={imageStyle}
                     />
+                    <img src="https://via.placeholder.com/400x400" className="display: none;"/>
+                    <div className={textStyle}>{pictures[2].title}</div>
                 </a>
             </Link>
         </div> 
@@ -67,58 +57,61 @@ export default function Gallery({ pictures }){
                         layout="fill"
                         objectFit="cover"
                         src={pictures[3].url}
-                        className={imageStyle}
                     />
+                    <img src="https://via.placeholder.com/400x400" className="display: none;"/>
+                    <div className={textStyle}>{pictures[3].title}</div>
                 </a>
             </Link>
         </div> 
         <div className={layoutSize2ARow}>
-            <Link href={`/projects/${pictures[0].slug}`} key={pictures[0].title}>
+            <Link href={`/projects/${pictures[4].slug}`} key={pictures[4].title}>
                 <a>
                     <ContentfulImage
                         layout="fill"
                         objectFit="cover"
-                        src={pictures[0].url}
-                        className={imageStyle}
+                        src={pictures[4].url}
                     />
+                    <img src="https://via.placeholder.com/800x400" className="display: none;"/>
+                    <div className={textStyle}>{pictures[4].title}</div>
                 </a>
             </Link>
         </div> 
         <div className={layoutSize2ACol}>
-            <Link href={`/projects/${pictures[1].slug}`} key={pictures[1].title}>
+            <Link href={`/projects/${pictures[5].slug}`} key={pictures[5].title}>
                 <a>
                     <ContentfulImage
                         layout="fill"
                         objectFit="cover"
-                        src={pictures[1].url}
-                        className={imageStyle}
+                        src={pictures[5].url}
                     />
+                    <img src="https://via.placeholder.com/400x800" className="display: none;"/>
+                    <div className={textStyle}>{pictures[5].title}</div>
                 </a>
             </Link>
         </div> 
         <div className={layoutSize2ACol}>
-            <Link href={`/projects/${pictures[2].slug}`} key={pictures[2].title}>
+            <Link href={`/projects/${pictures[6].slug}`} key={pictures[6].title}>
                 <a>
-                <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">{pictures[2].title}</div>
                     <ContentfulImage
                         layout="fill"
                         objectFit="cover"
-                        src={pictures[2].url}
-                        className={imageStyle}
+                        src={pictures[6].url}
                     />
+                    <div className={textStyle}>{pictures[6].title}</div>
+                    <img src="https://via.placeholder.com/400x800" className="display: none;"/>
                 </a>
             </Link>
         </div> 
         <div className={layoutSize2ACol}>
-            <Link href={`/projects/${pictures[3].slug}`} key={pictures[3].title}>
+            <Link href={`/projects/${pictures[7].slug}`} key={pictures[7].title}>
                 <a>
                     <ContentfulImage
                         layout="fill"
                         objectFit="cover"
-                        src={pictures[3].url}
-                        className={imageStyle}
+                        src={pictures[7].url}
                     />
-                    <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-4xl text-white font-semibold">{pictures[3].title}</div>
+                    <div className={textStyle}>{pictures[7].title}</div>
+                    <img src="https://via.placeholder.com/400x800" className="display: none;"/>
                 </a>
             </Link>
         </div> 
