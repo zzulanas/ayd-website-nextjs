0a9c3cbb7b44f706dd8d4fc33f70d04bd8ee81bf
zzulanas <zzulanas@gmail.com>
2023-11-01 15:28:19 -0400

    fix; I wish I could test vercel builds locally

--- types/contentful/TypeAboutPage.ts
+++ types/contentful/TypeAboutPage.ts
@@ -1,7 +1,10 @@
 import type { Entry, EntryFields } from "contentful";
 
 export interface TypeAboutPageFields {
+  fields: {
     content?: EntryFields.RichText;
+  };
+  contentTypeId?: string;
 }
 
 export type TypeAboutPage = Entry<TypeAboutPageFields>;
