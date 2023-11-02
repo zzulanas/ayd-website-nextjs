c67ebaaf7305e6709c83815b28891a73f4f20ff6
Zach Zulanas <zzulanas@gmail.com>
2023-11-02 11:26:49 -0400

    fix: Update meta.tsx AGAIN

--- components/meta.tsx
+++ components/meta.tsx
@@ -26,11 +26,11 @@ export default function Meta({ title, description, imageUrl, url }: MetaProps) {
       <meta property="og:description" content={description || OG_DESCRIPTION} />
       <meta
         property="og:url"
-        content={url || `https://${process.env.OG_IMAGE_URL}`}
+        content={url || `https://arq-y-di.com`}
       />
       <meta
         property="og:image"
-        content={imageUrl || `https://${process.env.OG_IMAGE_URL}/api/og`}
+        content={imageUrl || `https://arq-y-di.com/api/og`}
       />
       <link
         rel="apple-touch-icon"
